77ebb9480a804011f787cf76cad9eaa8d9ea2a0e
Mike Henke <henke.mike@gmail.com>
2013-02-09 17:15:36 -0600

    updating

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -48,7 +48,7 @@ class ActivitiesController < ApplicationController
 
     respond_to do |format|
       if @activity.save
-        format.html { redirect_to @activity, notice: 'Activity was successfully created.' }
+        format.html { redirect_to destination_activity_path(@destination.id, @activity), notice: 'Activity was successfully created.' }
         format.json { render json: @activity, status: :created, location: @activity }
       else
         format.html { render action: "new" }
@@ -65,7 +65,7 @@ class ActivitiesController < ApplicationController
 
     respond_to do |format|
       if @activity.update_attributes(params[:activity])
-        format.html { redirect_to @activity, notice: 'Activity was successfully updated.' }
+        format.html { redirect_to destination_activity_path(@destination.id, @activity), notice: 'Activity was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
