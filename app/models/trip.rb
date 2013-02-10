3285ec717622bc5b6a3db572d0bf4fb165ed4293
Mike Henke <henke.mike@gmail.com>
2013-02-09 21:32:10 -0600

    set up relationships with user

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,4 +4,6 @@ class Trip < ActiveRecord::Base
   has_many :destinations
 
   validates_presence_of :name
+
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130210024924) do
+ActiveRecord::Schema.define(:version => 20130210032238) do
 
   create_table "activities", :force => true do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(:version => 20130210024924) do
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
