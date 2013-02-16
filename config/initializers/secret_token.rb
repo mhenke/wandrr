6d1ac6c4c662f3b65628824a589475799cc696b0
Mike Henke <henke.mike@gmail.com>
2013-02-15 18:08:05 -0600

    changes to gemfile

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Wandrr::Application.config.secret_token = '3340dfd289a58fa5e625a85f0d4a54a5b7769acee6eee709e736d067f378f9fa3ef367ef9a0488c55b5595cfb4248e32775feef429a96098b56bbd458c02b822'
+Wandrr::Application.config.secret_token = ENV["TWITTER_CONSUMER_KEY"]
