b27a6b86a33887e5959087989f6775aa94a42d9f
Mike Henke <henke.mike@gmail.com>
2013-02-16 14:44:14 -0600

    adding ajax

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -33,6 +33,7 @@ class TripsController < ApplicationController
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @trip }
+      format.js # new.js.erb
     end
   end
 
@@ -51,9 +52,11 @@ class TripsController < ApplicationController
       if @trip.save
         format.html { redirect_to @trip, notice: 'Trip was successfully created.' }
         format.json { render json: @trip, status: :created, location: @trip }
+        format.js
       else
         format.html { render action: "new" }
         format.json { render json: @trip.errors, status: :unprocessable_entity }
+        format.js
       end
     end
   end
@@ -83,6 +86,7 @@ class TripsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to trips_url }
       format.json { head :no_content }
+      format.js
     end
   end
 
