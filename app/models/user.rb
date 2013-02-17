9add908e1e1ce0b6dab4668f54d63c70a62ce948
Mike Henke <henke.mike@gmail.com>
2013-02-16 19:32:47 -0600

    more twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,12 @@ class User < ActiveRecord::Base
   # attr_accessible :title, :body
   has_many :trips
   has_many :destinations, through: :trips
+
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.username = auth.info.nickname
+    end
+  end
 end
